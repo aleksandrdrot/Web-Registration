58325ffbd0809c790ef178799e5179b772627f6a
aleksandrdrot <aleksandr_drot@rambler.ru>
2023-03-29 22:55:52 +0300

    Registration

---
+++ src/main/java/com/drot/registration/WebRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.drot.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRegistrationApplication.class, args);
+	}
+
+}
